d01f185a4ec5146c1a1ae6c992ee19194410986d
Zaky Gusti <181220091@unmuhpnk.ac.id>
2024-05-15 07:22:15 +0700

    crate add fitur

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 
 const groceryItems = [
@@ -12,7 +13,7 @@ const groceryItems = [
     id: 2,
     name: 'Gula Pasir',
     quantity: 5,
-    checked: false,
+    checked: true,
   },
   {
     id: 3,
@@ -28,37 +29,62 @@ function Header() {
 }
 
 
-function Form() {
- return(
-  <form className="add-form">
-    <h3>Hari ini belanja apa kita?</h3>
+function Form({onAddItem}) {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i+1} key={i+1}>{i+1}</option>
+  ));
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return
+    const newItem = {
+      name:name, quantity:quantity, checked: false, id:Date.now(),
+    }; 
+
+    onAddItem(newItem);
+
+    console.log(newItem);
+    setName('')
+    setQuantity(1)
+    
+  }
+
+  return(
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        <input type="text" placeholder="nama barang..." />
+        <select onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
       </div>
-    <button>Tambah</button>
-  </form>
- );
+      <button>Tambah</button>
+    </form>
+  );
 }
 
+function Item({item}) {
+  return(
+    <li key={item.id}>
+      <input type="checkbox" />
+        <span style={item.checked ? { textDecoration: 'line-through' } : {}}>
+          {item.quantity} {item.name}
+        </span>
+      <button>&times;</button>
+    </li>
+  );
+}
 
-function GroceryList(){
+function GroceryList({items}){
   return(
     <>
       <div className="list">
         <ul>
-          {groceryItems.map((item) => (
-            <li key={item.id}>
-              <input type="checkbox" />
-              <span>{item.quantity} {item.name}</span>
-            <button>&times;</button>
-          </li>
+          {items.map((item) => (
+            <Item item={item} key={item.id}/>
           ))}
         </ul>
       </div>
@@ -74,16 +100,24 @@ function GroceryList(){
   );
 }
 
+
+
 function Footer() {
   <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
 }
 
 function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function  handleAddItem(item) {
+    setItems([...items, item])
+  }
+
   return (
     <div className="app">
       <Header/>
-      <Form/>
-      <GroceryList/>
+      <Form onAddItem={handleAddItem}/>
+      <GroceryList items={items}/>
       <Footer/>
     </div>
   );
@@ -91,4 +125,4 @@ function App() {
 
 export default App;
 
-// 27:29 : https://www.youtube.com/watch?v=HX2kAHnCEjY
+// 43:01 : https://www.youtube.com/watch?v=HX2kAHnCEjY
